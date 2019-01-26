d5b032fc2e5d371222a776b2cae2a03ad4b8f2fa
FujinagaTakashi <fujinaga.3210.takashi@gmail.com>
2019-01-26 23:48:04 +0900

    add AC code

--- abc116/abc116_d.cpp
+++ abc116/abc116_d.cpp
@@ -1,99 +1,68 @@
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <algorithm>
-#include <map>
+//submisssions/4054148を参考
+
+#include <bits/stdc++.h>
+
 using namespace std;
-
-int K[100001] = {0}; //各寿司の数
-unsigned long long sum[100001] = {0};
-int var = 0;
-bool more_2nd(pair<int, int> one, pair<int, int> ano)
-{
-    if (one.second != ano.second)
-    {
-        return one.second > ano.second;
-    }
-    else
-    {
-        return one.first > ano.first;
-    }
-}
-
+ 
+typedef long long ll;
+ 
+ 
 int main()
 {
     int n, k;
-    vector<pair<int, int>> p;
     cin >> n >> k;
+    //二次元動的配列の使いかた
+    vector<ll> a(n), b(n);
+    vector<vector<ll>> v(n);
 
-    for (int i = 0; i < n; i++)
-    {
-        int tmp1, tmp2;
-        cin >> tmp1 >> tmp2;
-        //std:make_pair
-        p.push_back(make_pair(tmp1, tmp2));
+    for(int i = 0; i < n; ++i){
+        cin >> a[i] >> b[i];
+        v[--a[i]].push_back(b[i]);
     }
-
-    sort(p.begin(), p.end(), more_2nd);
-
-    //初期評価値を導出
-
-    vector<pair<int, int>> pp; //部分列を作成
-    for (int i = 0; i < k; i++)
+    //for(auto& x : v){
+    //    sort(x.begin(), x.end(), greater<ll>());
+    //}
+    for(int i = 0; i < n; i++)
     {
-        pp.push_back(make_pair(p[i].first, p[i].second));
+        sort(v[i].begin(),v[i].end(), greater<ll>());
     }
+    
 
-    unsigned long long firstsum = 0;
-    for (int i = 0; i < pp.size(); i++)
+    vector<ll> top;
+    vector<ll> notop;
+
+    for(int i = 0; i < n; i++)
     {
-        if (K[pp[i].first] == 0)
-            var++;
-        K[pp[i].first]++;
-        firstsum += (unsigned long long)pp[i].second;
-    }
-    sum[var] = firstsum + var * var;
-
-    //変更
-    unsigned long long ans = sum[var];
-    for (int i = k; i < n; i++)
-    {
-        //cout << sum[var] << endl;
-
-        //cout << pp.size();
-        //cout << p[i].first << " " << p[i].second << " sum:" << sum[var] << " var:" << var <<  " K[p[i].first]:" << K[p[i].first] <<endl;
-        int tmp = -1;
-        sort(pp.begin(), pp.end(), more_2nd);
-        for (int j = pp.size() - 1; j >= 0; j--)
+        if(v[i].empty())
+            continue;
+        top.push_back(v[i][0]);
+        for(int j = 1; j < v[i].size(); j++)
         {
-            //cout << "r" << j;
-            //重複を検知
-            if (K[pp[j].first] >= 2)
-            {
-                tmp = j;
+            notop.push_back(v[i][j]);            
+        }
+    }
+    
+    sort(top.begin(), top.end(), greater<ll>());
+    sort(notop.begin(), notop.end(), greater<ll>());
+
+    ll ans = 0;
+    for(ll i = 0; i <= k; i++)
+    {
+        if (i > top.size() || (k - i) > notop.size()) continue;
+
+        ll tmp = 0;
+        for(ll j = 0; j < k; j++)
+        {
+
+            if (j < i){
+                tmp += top[j];
+            }else{
+                tmp += notop[j - i];
             }
         }
-        //重複ない場合、終わり
-        if (tmp == -1)
-        {
-            //cout << "break" << endl;
-            break;
-        }
-
-        //寿司が新種であること
-        if (K[p[i].first] == 0)
-        {
-            //cout << "change" << endl;
-            pp.erase(pp.begin() + tmp);
-            pp.push_back(make_pair(p[i].first, p[i].second));
-            K[tmp]--;
-            K[p[i].first]++;
-            var++;
-            sum[var] += sum[var - 1] + (var * 2 - 1) - (pp[tmp].second - p[i].second);
-            ans = max(sum[var], ans);
-        }
-
+        //int iだとWAになった。iは10^5まで取りうるの注意
+        tmp += i*i;
+        ans = max(ans, tmp);  
     }
-
     cout << ans << endl;
 }
