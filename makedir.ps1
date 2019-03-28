58211b197a26eb7942577ff2e24e7cda1e190dfe
FujinagaTakashi <fujinaga.3210.takashi@gmail.com>
2019-03-28 22:05:24 +0900

    <env>add tool to make new files

---
+++ makedir.ps1
@@ -0,0 +1,8 @@
+# Ex. powershell -ExecutionPolicy RemoteSigned .\makedir.ps1 abc123
+$contest_name = $Args[0]
+mkdir $contest_name
+$array = @("a","b","c","d")
+for ($i = 0; $i -lt $array.Count; $i++) {
+    $name_diff = $array[$i]
+    Copy-Item Template.cpp "${contest_name}/${contest_name}_${name_diff}.cpp"   
+}
