964326ecdc804a4f096b8fbb1771417e67c16379
FujinagaTakashi <fujinaga.3210.takashi@gmail.com>
2019-07-24 22:44:42 +0900

    change template file

--- Template.cpp
+++ Template.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define MOD 1000000007
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 typedef long long ll;
+
 
 int main()
 {
     int n;
     cin >> n;
+    vector<int> a;
+    rep(i, n){ int tmp; cin >> tmp; a.push_back(tmp);}
 }
 
