7d92d718171eece880cdfa5628beb1dc87b80411
FujinagaTakashi <fujinaga.3210.takashi@gmail.com>
2020-01-12 16:51:04 +0900

    AC code and update summary

--- dwacon/dwacon_b.cpp
+++ dwacon/dwacon_b.cpp
@@ -62,41 +62,30 @@ int main()
     vector<ll> x;
     rep(i, n){ ll tmp; cin >> tmp; x.push_back(tmp);}
 
-    vector<mint> sum;
-    mint sum_cnt = 0;
+    vector<ll> sum;
+    ll sum_cnt = 0;
     rep(i, n + 1) {
         sum.push_back(sum_cnt);
         sum_cnt += x[i];
     }
 
-    cout << endl;
-
-    mint ans_m(0);
-    mint mut(1);
+    mint ans_m(0); //答え
+    mint mut(1); //(n-1)!除算
     for (ll i = 1; i < n; i++)
     {
         mint m(i);
         mut *= m;
     }
+    mint sumprob(0);
     rep(i, n - 1){
         ll ip = i + 1;
-        mint part_m(0);
-        mint mutbuf(mut);
-
-        for (int j = i + 1; j < n; j++)
-        {
-            mint m2(2);
-            mint ppart_m(0);
-            ppart_m += x[j] - x[i];
-            cout << "::" << ppart_m.x << endl;
-            ppart_m /= (j == n-1) ? m2.pow(n-(ip + 1)) : m2.pow(j - i);
-            mint aa = (j == n-1) ? m2.pow(n-(ip + 1)) : m2.pow(j - i);
-            cout << ":" << aa.x << endl;
-            part_m += ppart_m;
-        }
-        part_m *= mut;
-        cout << "num?:" <<part_m.x << endl;
-        ans_m += part_m;
+        mint ans_part(0);
+        mint line = x[ip] - x[i];
+        sumprob += (mint)1/(mint)ip;
+        ans_part = sumprob * line;
+        ans_part *= mut;
+        //cout << ans_part.x << endl;
+        ans_m += ans_part;
     }
 
     cout << ans_m.x << endl;
